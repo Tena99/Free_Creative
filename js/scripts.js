704c054072b671fa12b7528874bde64d9145c617
Tena99 <strelka1515@gmail.com>
2022-07-07 15:32:05 +0300

    Added fonts, images, header, owl-carousel

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    nav: true,
+    navText: ["", " "],
+  });
+});
